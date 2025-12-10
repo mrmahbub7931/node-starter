d6a8a748ea815de7bc9837b598d33d881310e6a0
Mahbubar Rahmn <md.mahbub7931@gmail.com>
2025-12-10 01:04:45 +0100

    add admin and seprate frontend

---
+++ admin/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <p className="text-3xl text-blue-600 mt-20 text-center font-bold">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
